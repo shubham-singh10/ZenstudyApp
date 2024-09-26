6a814c1362450fd45495f5df3c7f99af48bc404a
Shubham Kumar Singh <shubhamsinghh9599@gmail.com>
2024-09-26 11:52:15 +0530

    icons

--- component/Header.jsx
+++ component/Header.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import {View, Text,} from 'react-native';
+import React from 'react';
+import { View, Text } from 'react-native';
 import styles from './Style';
 
 function Header() {
   return (
     <View style={styles.container}>
-    <View style={styles.header}>
-          <View>
+      <View style={styles.header}>
+        <View>
           <Text style={styles.logoh1}>ZenStudy.</Text>
           <Text style={styles.logoh2}>Make Education Imaginative</Text>
-          </View>
-          <Text style={styles.helpText}>Help</Text>
+        </View>
+        <Text style={styles.helpText}>Help</Text>
+      </View>
     </View>
-    </View>
-  )
+  );
 }
 
-export default Header
+export default Header;
